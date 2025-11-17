3a7dd6e236860df4bb918953d8e9623a9a53f151
Andriy Karpiuk <andrijkarpuk93@gmail.com>
2025-11-17 13:39:22 +0200

    Added modulo operation to calculator

--- calculator.py
+++ calculator.py
@@ -1,52 +1,54 @@
 def add(a, b):
- """Додавання двох чисел"""
- return a + b
+    """Додавання двох чисел"""
+    return a + b
+
 def subtract(a, b):
- """Віднімання двох чисел"""
- return a - b
+    """Віднімання двох чисел"""
+    return a - b
+
 def multiply(a, b):
- """Множення двох чисел"""
- return a * b
+    """Множення двох чисел"""
+    return a * b
+
 def divide(a, b):
- """Ділення двох чисел"""
- if b != 0:
- return a / b
- else:
- return "Помилка: ділення на нуль!"
+    """Ділення двох чисел"""
+    if b != 0:
+        return a / b
+    else:
+        return "Помилка: ділення на нуль!"
+
 def power(a, b):
- """Піднесення числа a до степеня b"""
- return a ** b
+    """Піднесення числа a до степеня b"""
+    return a ** b
+
+def modulo(a, b):
+    """Остача від ділення"""
+    if b != 0:
+        return a % b
+    else:
+        return "Помилка: ділення на нуль!"
+
 print("=== Простий калькулятор ===")
-print("Операції: +, -, *, /, **")
+print("Операції: +, -, *, /, **, %")
 print("Для виходу введіть 'exit'")
+
 while True:
- operation = input("\nВведіть операцію (+, -, *, /, **) або
-'exit': ")
+    operation = input("\nВведіть операцію (+, -, *, /, **, %) або 'exit': ")
 
- if operation.lower() == 'exit':
- print("До побачення!")
- break
+    if operation.lower() == 'exit':
+        print("До побачення!")
+        break
 
- if operation not in ['+', '-', '*', '/', '**']:
- print("Невірнв операція!")
-continue
+    if operation not in ['+', '-', '*', '/', '**', '%']:
+        print("Невірна операція!")
+        continue
 
- try:
- num1 = float(input("Введіть перше число: "))
- num2 = float(input("Введіть друге число: "))
+    try:
+        num1 = float(input("Введіть перше число: "))
+        num2 = float(input("Введіть друге число: "))
 
- if operation == '+':
- result = add(num1, num2)
- elif operation == '-':
- result = subtract(num1, num2)
- elif operation == '*':
- result = multiply(num1, num2)
- elif operation == '/':
- result = divide(num1, num2)
- elif operation == '**':
- result = power(num1, num2)
-
- print(f"Результат: {result}")
-
- except ValueError:
- print("Помилка: введіть коректні числа!")
+        if operation == '+':
+            result = add(num1, num2)
+        elif operation == '-':
+            result = subtract(num1, num2)
+        elif operation == '*':
